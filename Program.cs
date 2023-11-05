7b69c31e7f77ccbdcf460b3fb2c3a79b4fc026ab
Raphael Bahr <raphabahr@gmail.com>
2023-11-05 19:49:01 -0300

    entrega desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("47-99998889", modelo: "Tejorola", imei: "55997733", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Excel");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("47-99999777", modelo: "Iphone plus", imei: "33355577", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Photoshop");
